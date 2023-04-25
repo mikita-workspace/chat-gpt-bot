b45ea55882327030974d9cb2aed925af433895d4
Mikita Kandratsyeu <nickondr.production@gmail.com>
2023-04-26 00:10:29 +0300

    chore: added welcome strings

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,22 @@
 import { Telegraf } from 'telegraf';
 import LocalSession from 'telegraf-session-local';
 import { message } from 'telegraf/filters';
-import { code } from 'telegraf/format';
 import { oggConverter, openAI } from './utils';
-import { INITIAL_SESSION, MessageRoles, TELEGRAM_TOKEN } from './constants';
+import {
+  INITIAL_SESSION,
+  MessageRoles,
+  TELEGRAM_TOKEN,
+  gptModel,
+} from './constants';
 import { BotContextType } from './types';
 
 const bot = new Telegraf<BotContextType>(TELEGRAM_TOKEN);
 
+// TODO: implement session with MongoDB Atlas
+// Local session
 bot.use(new LocalSession({ database: 'session/session.json' }).middleware());
 
+// Command actions
 bot.command('start', async (ctx) => {
   ctx.session = INITIAL_SESSION;
   await ctx.reply('Waiting for a text or voice message...');
@@ -20,6 +27,18 @@ bot.command('new', async (ctx) => {
   await ctx.reply('Waiting for a text or voice message...');
 });
 
+bot.command('about', async (ctx) =>
+  ctx.reply(
+    `Built on the ${gptModel} architecture.\n\nhttps://github.com/mikita-kandratsyeu/telegram-bot`,
+  ),
+);
+
+bot.command('description', async (ctx) => {
+  ctx.reply(`Hi there 👋🏻\nI'm ChatGPT, an intelligent bot capable of responding to various user requests, including voice input. I'm built on the ${gptModel} architecture and possess extensive knowledge in various areas such as science, technology, arts, sports, health, business, and more. I can answer users' questions, help solve problems, and also engage in casual conversation on any topic. My interface allows for both text and voice input, making communication with me even more convenient and accessible for users.
+  `);
+});
+
+// Message actions
 bot.on(message('text'), async (ctx) => {
   ctx.session ??= INITIAL_SESSION;
 
