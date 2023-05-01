fe2dc54d012df32f55a27bb0d7526725f3ef4a32
Mikita Kandratsyeu <nickondr.production@gmail.com>
2023-05-01 10:26:16 +0300

    chore: changed model for user

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -2,6 +2,7 @@ import { Schema, model } from 'mongoose';
 
 const schema = new Schema({
   username: { type: String, required: true, unique: true },
+  role: { type: String, required: true },
 });
 
 export const UserModel = model('User', schema);
