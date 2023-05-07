9f2953d3cd96b213da5b87853c14819e911a8af1
Mikita Kandratsyeu <nickondr.production@gmail.com>
2023-05-07 23:35:29 +0300

    added normalize for session

--- src/middlewares/normalize.middleware.ts
+++ src/middlewares/normalize.middleware.ts
@@ -1,10 +1,16 @@
 import { BotContextType, GrammyMiddlewareFn } from '../types';
+import { MAX_SESSION_MESSAGES } from '../constants';
 
 export const normalize = (): GrammyMiddlewareFn<BotContextType> => async (ctx, next) => {
   const username =
     ctx?.update?.message?.from?.username ?? ctx?.update?.callback_query?.from?.username ?? '';
+  const sessionMessages = ctx.session.messages;
 
   ctx.session.username ??= username;
+
+  if (sessionMessages.length >= MAX_SESSION_MESSAGES) {
+    ctx.session.messages = sessionMessages.slice(Math.floor(MAX_SESSION_MESSAGES / 3));
+  }
 
   return next();
 };
