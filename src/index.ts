f0731ff75a44a5e3d3ca725412ce744e9ad06506
Mikita Kandratsyeu <nickondr.production@gmail.com>
2023-05-08 18:40:10 +0300

    add common error handling

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,31 @@
 import mongoose from 'mongoose';
 import { config } from './config';
 import { createBot } from './bot';
+import { GrammyError, HttpError } from 'grammy';
 
 const botInitialize = async () => {
-  try {
-    await mongoose.connect(config.MONGODB_URI);
+  await mongoose.connect(config.MONGODB_URI);
 
-    const bot = createBot();
+  const bot = createBot();
 
-    bot.start();
+  bot.start();
 
-    process.once('SIGINT', () => bot.stop());
-    process.once('SIGTERM', () => bot.stop());
-  } catch (error) {
-    console.error(`ERROR::botInitialize::${(error as Error).message}`);
-  }
+  // TODO: fix in Error Report feat
+  bot.catch((err) => {
+    const ctx = err.ctx;
+    console.error(`Error while handling update ${ctx.update.update_id}:`);
+    const e = err.error;
+    if (e instanceof GrammyError) {
+      console.error('Error in request:', e.description);
+    } else if (e instanceof HttpError) {
+      console.error('Could not contact Telegram:', e);
+    } else {
+      console.error('Unknown error:', e);
+    }
+  });
+
+  process.once('SIGINT', () => bot.stop());
+  process.once('SIGTERM', () => bot.stop());
 };
 
 botInitialize();
