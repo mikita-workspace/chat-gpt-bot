ef55fb734cce9727cc5707b4f019596248bb1fa7
Mikita Kandratsyeu <40242552+mikita-kandratsyeu@users.noreply.github.com>
2023-05-14 20:29:31 +0300

    bug: updated gpt limit for session messages (#25)

--- src/middlewares/normalize.middleware.ts
+++ src/middlewares/normalize.middleware.ts
@@ -9,7 +9,7 @@ export const normalize = (): GrammyMiddlewareFn<BotContextType> => async (ctx, n
   ctx.session.username ??= username;
 
   if (sessionMessages.length >= MAX_SESSION_MESSAGES) {
-    ctx.session.messages = sessionMessages.slice(Math.floor(MAX_SESSION_MESSAGES / 3));
+    ctx.session.messages = sessionMessages.slice(Math.floor(MAX_SESSION_MESSAGES / 2.5));
   }
 
   return next();
