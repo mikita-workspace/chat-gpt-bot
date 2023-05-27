3b87f26cb2a35a8e746854e67509dea4a2b31852
Mikita Kandratsyeu <nickondr.production@gmail.com>
2023-05-27 20:58:06 +0300

    add webhook

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import { createBot } from '@bot/bot';
 import { config } from '@bot/config';
 import { run } from '@grammyjs/runner';
+import express from 'express';
+import { webhookCallback } from 'grammy';
 import mongoose from 'mongoose';
 
 const botInitialize = async () => {
@@ -8,7 +10,21 @@ const botInitialize = async () => {
 
   const bot = createBot();
 
-  run(bot);
+  if (process.env.NODE_ENV === 'production') {
+    // Use Webhooks for the production server
+    const app = express();
+
+    app.use(express.json());
+    app.use(webhookCallback(bot, 'express'));
+
+    const PORT = process.env.PORT || 3000;
+    app.listen(PORT, () => {
+      console.log(`Bot listening on port ${PORT}`);
+    });
+  } else {
+    // Use Long Polling for development
+    run(bot);
+  }
 
   process.once('SIGINT', () => bot.stop());
   process.once('SIGTERM', () => bot.stop());
