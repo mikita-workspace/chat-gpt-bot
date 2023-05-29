4098f5be8560e649a3b11360cc087a43e0515625
Mikita Kandratsyeu <nickondr.production@gmail.com>
2023-05-29 23:49:48 +0300

    bug: fixed an issue with blocking bot by other user

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -1,35 +1,43 @@
 import { config } from '@bot/config';
 import { BotCommands, UserRoles } from '@bot/constants';
-import { mongo } from '@bot/services';
+import { logger, mongo } from '@bot/services';
 import { BotContextType, GrammyMiddlewareFn } from '@bot/types';
 
 export const auth = (): GrammyMiddlewareFn<BotContextType> => async (ctx, next) => {
-  const username = String(ctx?.from?.username);
-  const action = String(ctx?.update?.message?.text);
+  try {
+    const username = String(ctx?.from?.username);
+    const action = String(ctx?.update?.message?.text);
 
-  if (username === config.SUPER_ADMIN_USERNAME) {
-    return next();
-  }
+    logger.defaultMeta = { username };
 
-  const user = await mongo.getUser(username);
-
-  if (user?.enabled) {
-    if (action === `/${BotCommands.ADMIN}` && user?.role !== UserRoles.ADMIN) {
-      await ctx.reply(ctx.t('error-message-auth-admin'));
-
-      return;
+    if (username === config.SUPER_ADMIN_USERNAME) {
+      return await next();
     }
 
-    if (action === `/${BotCommands.MODERATOR}` && user?.role !== UserRoles.MODERATOR) {
-      await ctx.reply(ctx.t('error-message-auth-moderator'));
+    const user = await mongo.getUser(username);
 
-      return;
+    if (user?.enabled) {
+      if (action === `/${BotCommands.ADMIN}` && user?.role !== UserRoles.ADMIN) {
+        await ctx.reply(ctx.t('error-message-auth-admin'));
+
+        return;
+      }
+
+      if (action === `/${BotCommands.MODERATOR}` && user?.role !== UserRoles.MODERATOR) {
+        await ctx.reply(ctx.t('error-message-auth-moderator'));
+
+        return;
+      }
+
+      return await next();
     }
 
-    return next();
+    await ctx.reply(ctx.t('error-message-auth'));
+
+    return;
+  } catch (error) {
+    logger.error(error.description);
+
+    return;
   }
-
-  await ctx.reply(ctx.t('error-message-auth'));
-
-  return;
 };
