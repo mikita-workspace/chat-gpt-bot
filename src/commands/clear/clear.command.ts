a001733c404c58185ba6d0db053b255cefdd09ae
Mikita Kandratsyeu <nickondr.production@gmail.com>
2023-10-04 20:22:27 +0300

    bug: fixed issues when user may get the wrong answer to the question from the GPT

--- src/commands/clear/clear.command.ts
+++ src/commands/clear/clear.command.ts
@@ -1,15 +1,13 @@
+import { updateUserConversationMessagesCallback } from '@bot/callbacks';
 import { BotCommands } from '@bot/constants';
 import { inlineGoToChat } from '@bot/keyboards';
-import { mongo } from '@bot/services';
 import { BotType } from '@bot/types';
 
 export const clearCommand = (bot: BotType) =>
   bot.command(BotCommands.CLEAR, async (ctx) => {
     const currentUsername = String(ctx.from?.username);
 
-    await mongo.updateUserConversation(currentUsername, ctx.session.user.messages);
-
-    ctx.session.user.messages = [];
+    await updateUserConversationMessagesCallback(ctx, currentUsername);
 
     await ctx.reply(ctx.t('info-message-clear-current-session'), {
       reply_markup: inlineGoToChat(ctx),
--- src/commands/text/text.command.ts
+++ src/commands/text/text.command.ts
@@ -1,3 +1,5 @@
+import { updateUserConversationMessagesCallback } from '@bot/callbacks';
+import { TEN_MIN_MS } from '@bot/constants';
 import { getGPTAnswer } from '@bot/helpers';
 import { inlineShareWithContacts } from '@bot/keyboards';
 import { logger } from '@bot/services';
@@ -8,6 +10,16 @@ export const textCommand = (bot: BotType) => {
     try {
       const messageId = Number(ctx?.message?.message_id);
       const text = String(ctx?.message?.text);
+      const username = String(ctx?.from?.username);
+
+      const currentTimestamp = Date.now();
+      const lastMessageTimestamp = Math.min(
+        ...ctx.session.user.messages.map(({ timestamp }) => new Date(timestamp).getTime()),
+      );
+
+      if (Math.abs(currentTimestamp - lastMessageTimestamp) >= TEN_MIN_MS) {
+        updateUserConversationMessagesCallback(ctx, username);
+      }
 
       const gptAnswer = (await getGPTAnswer(ctx, text)) ?? '';
 
