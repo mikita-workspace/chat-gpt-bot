120bc016fc6f904218ad873eb6999c8a1b8fc99c
Mikita Kandratsyeu <nickondr.production@gmail.com>
2023-11-11 21:29:02 +0300

    feat: enable mongo logger

--- src/services/logger.service.ts
+++ src/services/logger.service.ts
@@ -2,8 +2,8 @@ import 'winston-mongodb';
 
 import { winstonConfig } from '@bot/common/constants';
 import { getTimestampUnix } from '@bot/common/utils';
-// import { config } from '@bot/config';
-// import { ClientLoggerModel } from '@bot/models';
+import { config } from '@bot/config';
+import { ClientLoggerModel } from '@bot/models';
 import { addColors, createLogger, format, Logger, transports } from 'winston';
 
 class LoggerService {
@@ -47,20 +47,19 @@ class LoggerService {
       ],
     });
 
-    // TODO: Temporary disabled
     // NOTE: Logger with mongoDB does not work on test environment for unit testing
-    // if (process.env.NODE_ENV !== 'test') {
-    //   this.logger.add(
-    //     new transports.MongoDB({
-    //       collection: ClientLoggerModel.collection.name,
-    //       db: config.MONGODB_URI,
-    //       format: format.metadata(),
-    //       options: {
-    //         useUnifiedTopology: true,
-    //       },
-    //     }),
-    //   );
-    // }
+    if (process.env.NODE_ENV !== 'test') {
+      this.logger.add(
+        new transports.MongoDB({
+          collection: ClientLoggerModel.collection.name,
+          db: config.MONGODB_URI,
+          format: format.metadata(),
+          options: {
+            useUnifiedTopology: true,
+          },
+        }),
+      );
+    }
 
     if (process.env.NODE_ENV !== 'production') {
       this.logger.add(
