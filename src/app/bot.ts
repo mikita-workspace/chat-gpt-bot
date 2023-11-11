32f1376d638c149cd8d87f6cb994e0a2ecdc0489
Mikita Kandratsyeu <mikita_kandratsyeu1@epam.com>
2023-11-11 11:51:03 +0300

    feat: temp disable

--- src/app/bot.ts
+++ src/app/bot.ts
@@ -1,6 +1,8 @@
 import { ModelGPT } from '@bot/api/gpt/constants';
 import { BotContextType } from '@bot/app/types';
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { BotLanguageCodes, botName } from '@bot/common/constants';
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { handleBotError, mapBotCommands, mapBotDescription } from '@bot/common/helpers';
 import { callbackQueryComposer, conversationComposer, sessionComposer } from '@bot/composers';
 import { config } from '@bot/config';
@@ -9,9 +11,12 @@ import { changeModule } from '@bot/modules/change';
 import { restartModule } from '@bot/modules/restart';
 import { textModule } from '@bot/modules/text';
 import { voiceModule } from '@bot/modules/voice';
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { hydrate } from '@grammyjs/hydrate';
 import { I18n } from '@grammyjs/i18n';
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { limit as rateLimit } from '@grammyjs/ratelimiter';
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { apiThrottler } from '@grammyjs/transformer-throttler';
 import { Bot } from 'grammy';
 import * as path from 'path';
@@ -32,21 +37,22 @@ export const createBot = () => {
     useSession: true,
   });
 
-  Object.values(BotLanguageCodes).forEach(async (languageCode) => {
-    await bot.api.setMyDescription(mapBotDescription(i18n, languageCode), {
-      language_code: languageCode,
-    });
+  // TODO: Will be enable in release-3.0.1
+  // Object.values(BotLanguageCodes).forEach(async (languageCode) => {
+  //   await bot.api.setMyDescription(mapBotDescription(i18n, languageCode), {
+  //     language_code: languageCode,
+  //   });
 
-    await bot.api.setMyCommands(mapBotCommands(i18n, languageCode), {
-      language_code: languageCode,
-    });
-  });
+  //   await bot.api.setMyCommands(mapBotCommands(i18n, languageCode), {
+  //     language_code: languageCode,
+  //   });
+  // });
 
-  bot.api.config.use(apiThrottler());
+  // bot.api.config.use(apiThrottler());
 
-  bot.use(rateLimit());
+  // bot.use(rateLimit());
 
-  bot.use(hydrate());
+  // bot.use(hydrate());
 
   bot.use(i18n);
 
