8dd4c8f0e3e0e908fbb3f3530c71d7981fd0fc96
Mikita Kandratsyeu <nickondr.production@gmail.com>
2023-11-13 22:24:49 +0300

    feat: updated logger service

--- src/services/logger.service.ts
+++ src/services/logger.service.ts
@@ -1,7 +1,6 @@
 import 'winston-mongodb';
 
 import { winstonConfig } from '@bot/common/constants';
-import { getTimestampUnix } from '@bot/common/utils';
 import { config } from '@bot/config';
 import { ClientLoggerModel } from '@bot/models';
 import { addColors, createLogger, format, Logger, transports } from 'winston';
@@ -28,20 +27,14 @@ class LoggerService {
           filename: 'errors.log',
           level: 'error',
           format: format.printf(
-            (info) =>
-              `${getTimestampUnix(info.timestamp)} ${info.level.toLocaleUpperCase()}: ${
-                info.message
-              }`,
+            (info) => `${info.timestamp} ${info.level.toLocaleUpperCase()}: ${info.message}`,
           ),
         }),
         new transports.File({
           filename: 'combined.log',
           level: 'silly',
           format: format.printf(
-            (info) =>
-              `${getTimestampUnix(info.timestamp)} ${info.level.toLocaleUpperCase()}: ${
-                info.message
-              }`,
+            (info) => `${info.timestamp} ${info.level.toLocaleUpperCase()}: ${info.message}`,
           ),
         }),
       ],
