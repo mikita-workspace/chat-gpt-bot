4a0c417b1cb38e9b49225fa539d170bf8b25f01f
Mikita Kandratsyeu <nickondr.production@gmail.com>
2023-11-14 17:08:33 +0300

    feat: code refactored

--- src/api/gpt/constants/index.ts
+++ src/api/gpt/constants/index.ts
@@ -1,6 +1,6 @@
 export const MODEL_GPT_DEFAULT = { title: 'GPT-3.5 Turbo', model: 'gpt-3.5-turbo-1106' };
-export const MODEL_SPEECH_DEFAULT = { title: 'Whisper', model: 'whisper-1' };
-export const MODEL_IMAGE_DEFAULT = { title: 'DALL·E 3', model: 'dall-e-3', max: 1 };
+export const MODEL_SPEECH_DEFAULT = { title: 'Whisper', model: 'whisper-1', max: 10 * 60 * 1000 };
+export const MODEL_IMAGE_DEFAULT = { title: 'DALL·E 2', model: 'dall-e-2', max: 3 };
 
 export enum TypeGPT {
   AUDIO = 'audio',
