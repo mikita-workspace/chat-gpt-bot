7f6857d8a650abfb61cc37fb30a8df8ab035711a
Mikita Kandratsyeu <nickondr.production@gmail.com>
2023-11-28 12:16:37 +0300

    chore: updated start command

--- src/modules/restart/restart.module.ts
+++ src/modules/restart/restart.module.ts
@@ -1,13 +1,11 @@
 import { BotType } from '@bot/app/types';
 import { BotCommand } from '@bot/common/constants';
-import { resetSelectedModel } from 'common/helpers';
 
 export const restartModule = (bot: BotType) =>
   bot.command(BotCommand.RESTART, async (ctx) => {
     const messageId = Number(ctx.message?.message_id);
 
     ctx.session.client.messages = [];
-    ctx.session.selectedModel = resetSelectedModel();
 
     return ctx.reply(ctx.t('restart-message'), {
       reply_to_message_id: messageId,
--- src/modules/start/start.module.ts
+++ src/modules/start/start.module.ts
@@ -1,9 +1,12 @@
 import { BotType } from '@bot/app/types';
 import { BotCommand } from '@bot/common/constants';
+import { resetSelectedModel } from '@bot/common/helpers';
 
 export const startModule = (bot: BotType) =>
   bot.command(BotCommand.START, async (ctx) => {
     const messageId = Number(ctx.message?.message_id);
+
+    ctx.session.selectedModel = resetSelectedModel();
 
     return ctx.reply(ctx.t('start-description'), {
       reply_to_message_id: messageId,
