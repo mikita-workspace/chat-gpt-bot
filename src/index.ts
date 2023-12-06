9cae20c6de3b0e0fd3993d3277cb7fffcdb0004f
Mikita Kandratsyeu <40242552+mikita-kandratsyeu@users.noreply.github.com>
2023-12-06 14:26:16 +0300

    chore: temp disable webhook for prod

--- src/index.ts
+++ src/index.ts
@@ -9,22 +9,23 @@ import { webhookCallback } from 'grammy';
 const botInitialize = async () => {
   const bot = createBot();
 
-  if (process.env.NODE_ENV === 'production') {
-    // Use Webhooks for the production server
-    // Source: https://grammy.dev/guide/deployment-types.html#long-polling-vs-webhooks
-    const app = express();
+  // Temporary disable webhook for prod
+  // if (process.env.NODE_ENV === 'production') {
+  //   // Use Webhooks for the production server
+  //   // Source: https://grammy.dev/guide/deployment-types.html#long-polling-vs-webhooks
+  //   const app = express();
 
-    app.use(express.json());
-    app.use(webhookCallback(bot, 'express', handleTimeoutError, WEBHOOK_TIMEOUT));
+  //   app.use(express.json());
+  //   app.use(webhookCallback(bot, 'express', handleTimeoutError, WEBHOOK_TIMEOUT));
 
-    app.listen(config.PORT, '0.0.0.0', () => {
-      // eslint-disable-next-line no-console
-      console.info(`Bot listening on port ${config.PORT}`);
-    });
-  } else {
+  //   app.listen(config.PORT, '0.0.0.0', () => {
+  //     // eslint-disable-next-line no-console
+  //     console.info(`Bot listening on port ${config.PORT}`);
+  //   });
+  // } else {
     // Use Long Polling for development
     run(bot);
-  }
+  // }
 
   process.once('SIGINT', () => bot.stop());
   process.once('SIGTERM', () => bot.stop());
